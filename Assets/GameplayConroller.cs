507af1a4d10c7da52d3f8b0b874da8120873c105
Cavid <hcavid386@gmail.com>
2021-09-17 12:31:51 +0400

    main

--- Assets/GameplayConroller.cs
+++ Assets/GameplayConroller.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameplayConroller : MonoBehaviour
 {
@@ -10,23 +12,48 @@ public class GameplayConroller : MonoBehaviour
     [SerializeField] private TextMeshProUGUI leveltext;
     [SerializeField] private TextMeshProUGUI equationtext;
     [SerializeField] private TextMeshProUGUI answertext;
+    [SerializeField] private GameObject mainMenuPanelpanel;
+
+    [SerializeField] private Text text;
+    
+
     private bool isclean;
 
     private int _levelindex;
     private void Start()
     {
+        mainMenuPanelpanel.SetActive(false);
+
         _levelindex = PlayerPrefs.GetInt("LevelIndex");
-        Debug.Log(_levelindex);
+        //Debug.Log(_levelindex);
         leveltext.text = "Level" + (_levelindex + 1);
         equationtext.text = QuestionsAnswers[_levelindex].Element[0];
-
     }
-
+    public void reloadmenubutoon()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void levelchancger(int i) 
+    {
+        leveltext.text = "Level" + (i + 1);
+        equationtext.text = QuestionsAnswers[i].Element[0];
+    }
     public void onEnterButoon()
     {
         if (answertext.text==QuestionsAnswers[_levelindex].Element[1])
         {
             Debug.Log("true");
+            text.color = Color.green;
+            text.text = "Cavab dogrudu";
+            answertext.text = "";
+            _levelindex++;
+            levelchancger(_levelindex);
+
+        }
+        else
+        {
+            mainMenuPanelpanel.SetActive(true);
+
         }
 
     }
@@ -37,6 +64,7 @@ public class GameplayConroller : MonoBehaviour
         {
             answertext.text = "";
             isclean = true;
+
 
         }
         answertext.text += number;
