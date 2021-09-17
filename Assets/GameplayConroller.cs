5a81fb52511bd3e5ac93bb1abeee52736450df8e
Cavid <hcavid386@gmail.com>
2021-09-17 11:47:17 +0400

    main

--- Assets/GameplayConroller.cs
+++ Assets/GameplayConroller.cs
@@ -6,8 +6,11 @@ using TMPro;
 public class GameplayConroller : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private ArrayElement[] arrayElements = new ArrayElement[2];
+    [SerializeField] private ArrayElement[] QuestionsAnswers = new ArrayElement[2];
     [SerializeField] private TextMeshProUGUI leveltext;
+    [SerializeField] private TextMeshProUGUI equationtext;
+    [SerializeField] private TextMeshProUGUI answertext;
+    private bool isclean;
 
     private int _levelindex;
     private void Start()
@@ -15,7 +18,33 @@ public class GameplayConroller : MonoBehaviour
         _levelindex = PlayerPrefs.GetInt("LevelIndex");
         Debug.Log(_levelindex);
         leveltext.text = "Level" + (_levelindex + 1);
+        equationtext.text = QuestionsAnswers[_levelindex].Element[0];
 
+    }
+
+    public void onEnterButoon()
+    {
+        if (answertext.text==QuestionsAnswers[_levelindex].Element[1])
+        {
+            Debug.Log("true");
+        }
+
+    }
+    public void onNumberButoonPresed(int number)
+    {
+       
+        if (!isclean)
+        {
+            answertext.text = "";
+            isclean = true;
+
+        }
+        answertext.text += number;
+    }
+    public void onCleanTextButtonPressed()
+    {
+        answertext.text = "Enter Text..";
+        isclean = false;
     }
 
 }
